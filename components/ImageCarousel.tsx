958d80926b30b3c8af196dc1012791be536dcf0c
Eug <eupston130@hotmail.om>
2021-11-23 22:43:45 -0500

    updated with initial carousel skeleton

--- components/ImageCarousel.tsx
+++ components/ImageCarousel.tsx
@@ -1,11 +1,51 @@
-import {NextPage} from "next";
-import styles from "../styles/ImageCarousel.module.css";
+import {useEffect, useState} from "react";
+import Image from 'next/image'
+import {inspect} from "util";
+import styles from "/styles/ImageCarousel.module.css"
+import leftArrow from "/public/LeftArrow.svg"
+import rightArrow from "/public/RightArrow.svg"
+
+interface CullyImage {
+    id: string,
+    filename: string,
+    url: string
+}
 
 const ImageCarousel = () => {
-
+    const [images, setImages] = useState<CullyImage[]>();
+    useEffect(() => {
+        const getAllImages = async () => {
+            const imageEndpoint = process.env.NEXT_PUBLIC_CULLY_API + "/images";
+            const res = await fetch(imageEndpoint);
+            const resJson = await res.json();
+            return resJson.data;
+        }
+        getAllImages()
+            .then(images => {
+                setImages(images);
+        })
+            .catch(error => {
+                console.error(error);
+        });
+    }, [])
     return(
-        <div>
-            Image Carousel
+        <div className={styles.carouselContainer} >
+            {
+                images ?
+                    <div>
+                        <div className={styles.imgContainer}>
+                            <Image src={images[1]?.url} width={926} height={618} />
+                        </div>
+                        <div className={styles.selectorContainer}>
+                            <Image src={leftArrow}/>
+                            <p>{images[1]?.filename}</p>
+                            <Image src={rightArrow}/>
+                        </div>
+                    </div>
+                    :
+                //put spinner here
+                null
+            }
         </div>
     )
 }
